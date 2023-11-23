4d8c7850c46887124274c83b7877b1271ea976fd
victor <victor_14x_02s@hotmail.com>
2023-11-22 23:13:10 -0300

    lo que pude hacer hasta ahora por que me da muchos errores

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Ecommerce</h1>
+      <section>
+        <button>bebidas</button>
+        <button>galletas</button>
+        <button>cigarros</button>
+        <button>varios</button>
+      </section>
+    </nav>
+  )
+}
+
+export default Navbar
